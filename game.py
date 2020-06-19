394fa66e92518e7a6690dc23adfafd8b96361c86
EJ Cox <coxej1@gmail.com>
2020-06-19 18:10:21 -0400

    added splash screen

--- game.py
+++ game.py
@@ -30,6 +30,18 @@ class Game:
 		frames = 0
 		last_frame = pygame.time.get_ticks()
 
+		# Splash Screen
+		# Get new window variable since doing self.renderer.window didn't work as expected
+		window = pygame.display.set_mode((self.renderer.screen_width, self.renderer.screen_height), pygame.RESIZABLE)
+		# While loop to display splash screen for 5 seconds
+		while pygame.time.get_ticks() < last_frame + 5000:
+			# Load image
+			startscreen = pygame.image.load('splash screen/covidsim.jpg')
+			# Draw image into window starting at positon 0, 0
+			window.blit(startscreen, (0, 0))
+			# Update display
+			pygame.display.flip()
+
 		# Game loop:
 		while running:
 			# 1. Handle input from the user interface
